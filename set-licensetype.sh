9c0a40c8edf546cea7b070fc199d499c0e656a77
Campbell Vertesi <campbell@vertesi.com>
2023-09-21 17:58:39 +0200

    Improve short vs long options

--- set-licensetype.sh
+++ set-licensetype.sh
@@ -22,18 +22,8 @@ set -eu
 # All parameters are required except offer-id.
 #
 
-# Print usage information.
-usage() {
-    echo "Usage: $0 --license-type [license type] --offer-id [optional offer id] --resource-group [resource group] --ids [TXT file with VM IDs]"
-    echo "Example: $0 --license-type RHEL_BYOS --resource-group myResourceGroup --ids vms.txt"
-    echo "Parameters:"
-    echo "  --license-type / -l :    The license type to set for the VMs. Possible values are: RHEL_BASE, RHEL_EUS, RHEL_SAPAPPS, RHEL_SAPHA, RHEL_BASESAPAPPS, RHEL_BASESAPHA, RHEL_BYOS"
-    echo "  --resource-group / -g :  The resource group containing the VMs."
-    echo "  --ids / -i :             A TXT file containing the VM IDs, one per line."
-    echo "  --offer-id / -o :        [OPTIONAL] If you bought Red Hat subscriptions from Microsoft, the Id of the Marketplace private offer."
-    echo ""
-    echo "All parameters are required except offer-id."
-}
+# Include helper functions.
+source ./includes.sh
 
 # Exit with usage information.
 exit_abnormal() {
@@ -46,18 +36,30 @@ license_type=""
 resource_group=""
 ids=""
 offer_id=""
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    '--license-type')   set -- "$@" '-l'   ;;
+    '--resource-group') set -- "$@" '-g'   ;;
+    '--ids')   set -- "$@" '-i'   ;;
+    '--offer-id')     set -- "$@" '-0'   ;;
+    "--"*)           usage ${arg}; exit 2;;
+    *)          set -- "$@" "$arg" ;;
+  esac
+done
 while getopts ":l:r:i:" arg; do
     case "${arg}" in
-        l | license-type)
+        l)
             license_type=${OPTARG}
             ;;
-        r | resource-group)
+        g)
             resource_group=${OPTARG}
             ;;
-        i | ids)
+        i)
             ids=${OPTARG}
             ;;
-        o | offer-id)
+        o)
             offer_id=${OPTARG}
             ;;
         esac
